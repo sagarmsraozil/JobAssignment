4d6268cee6c8e59f2d8f5d312277c37fcddacda9
sagar <sagarcrcoc@gmail.com>
2021-10-01 17:08:44 +0545

    Category api done

--- controllers/categoryController.js
+++ controllers/categoryController.js
@@ -1,5 +1,3 @@
-//third party modules
-
 //models
 const CompanyCategory = require('../models/companyCategoryModel');
 
@@ -27,7 +25,7 @@ module.exports.addCategory = async(req,res)=>{
 
             let storeCategory = await categoryObj.save();
             
-            if(storeCategory != undefined)
+            if(storeCategory != null)
             {
                 return res.status(200).json({'success':true,'message':"Category Added!!",'data':storeCategory});
             }
@@ -78,3 +76,79 @@ module.exports.fecthSingleCategory = async(req,res)=>{
     }
 }
 
+//API: To update category
+module.exports.updateCategory = async(req,res)=>{
+    try{
+        let categoryId = req.params.id;
+        let title = req.body['title'].trim();
+
+        let category = await CompanyCategory.findOne({'_id':categoryId});
+        if(category != null)
+        {   
+             //filtering data to check whether the given title already exists or not.
+             let categories = await CompanyCategory.find({"_id":{$ne:category._id}});
+             let categoryContainer = categories.map((val)=>{return val.title.toLowerCase().trim()});
+
+            if(categoryContainer.includes(title.toLowerCase()))
+            {
+                return res.status(202).json({'success':false,'message':"Similar title already exists."});
+            }   
+            else
+            {
+                let updateCategory = await CompanyCategory.updateMany({"_id":category._id},{
+                    $set:{
+                        "title":title,
+                        "updated_at":new Date()
+                    }
+                })
+
+                
+
+                if(updateCategory.modifiedCount >= 1)
+                {
+                    return res.status(200).json({'success':true,'message':"Category Updated!!"});
+                }
+                else
+                {
+                    return res.status(202).json({'success':false,'message':"Failed To Update!!"});
+                }
+            }
+        }
+        else
+        {
+            return res.status(202).json({"success":false,'message':"Category Unavailable!!"})
+        }
+    }
+    catch(err)
+    {
+        return res.status(404).json({'success':false,'message':err});
+    }
+}
+
+//API: To delete category
+module.exports.deleteCategory = async(req,res)=>{
+    try{
+        let categoryId = req.params.id;
+
+        let category = await CompanyCategory.findOne({'_id':categoryId});
+        if(category != null)
+        {   
+             let deleteCategory = await CompanyCategory.deleteOne({'_id':category._id});
+             if(deleteCategory.deletedCount >= 1)
+             {
+                return res.status(200).json({'success':true,'message':"Category Removed!!"});
+             }
+             else
+             {
+                 return res.status(202).json({'success':false,'message':"Failed To Delete!!"});
+             }
+        }
+        else
+        {
+            return res.status(202).json({"success":false,'message':"Category Unavailable!!"})
+        }
+    }
+    catch(err) {
+        return res.status(404).json({'success':false,'message':err});
+    }
+}
--- routes/categoryRoute.js
+++ routes/categoryRoute.js
@@ -3,14 +3,18 @@ const express = require('express');
 const router = express.Router();
 
 //local modules
-const {addCategory,fetchCategory} = require('../controllers/categoryController');
+const {addCategory,fetchCategory,fecthSingleCategory,updateCategory,deleteCategory} = require('../controllers/categoryController');
 
 //Route: To add a new category.
 router.post('/',addCategory);
 //Route: To fetch 10 company categories.
 router.get('/',fetchCategory);
 //Route: To fetch single category.
-
+router.get('/:id',fecthSingleCategory);
+//Route: To update category.
+router.put('/:id',updateCategory);
+//Route: To delete category.
+router.delete('/:id',deleteCategory);
 
 
 module.exports = router;
